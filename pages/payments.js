17a289ad3b2ef35bc3707d169dcf08ce89ca6ad6
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-30 13:45:03 +0800

    completed UI

--- pages/payments.js
+++ pages/payments.js
@@ -1,12 +1,23 @@
+import { Button } from "@mui/material";
 import React from "react";
 import Layout from "../component/Layout";
 import paymentStyles from "./css/payment.module.css";
+import useStyles from "../utils/styles";
 
 export default function Payments() {
+  const styles = useStyles();
   return (
     <Layout>
       <div>
-        <div className={paymentStyles.paymentContainer}>x</div>
+        <div className={paymentStyles.paymentContainer}>
+          <div className={paymentStyles.paymentButtonContainer}>
+            <button className={paymentStyles.buttons}>PAYMENT HISTORY</button>
+            <button className={paymentStyles.buttons}>
+              REQUEST WITHDRAWAL
+            </button>
+            <button className={paymentStyles.buttons}>CLIENT PAYMENTS</button>
+          </div>
+        </div>
       </div>
     </Layout>
   );
