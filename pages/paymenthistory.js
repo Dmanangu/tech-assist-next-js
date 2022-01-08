735abf05d9dff490749bcc137841b2e6b40fbf93
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2022-01-08 22:50:06 +0800

    added firebase

--- pages/paymenthistory.js
+++ pages/paymenthistory.js
@@ -1,16 +1,17 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React from "react";
 import Layout from "../component/Layout";
 import paymentStyles from "./css/payment.module.css";
+import paymentHistoryStyles from "./css/paymenthistory.module.css";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
 
-export default function Payments() {
+export default function PaymentHistory() {
   const styles = useStyles();
   return (
     <Layout>
       <div>
-        <div className={paymentStyles.paymentContainer}>
+        <div className={paymentHistoryStyles.paymentHistoryContainer}>
           <div className={paymentStyles.paymentButtonContainer}>
             <NextLink href={"/paymenthistory"} passHref>
               <button className={paymentStyles.buttons}>PAYMENT HISTORY</button>
@@ -23,6 +24,12 @@ export default function Payments() {
             <NextLink href={"/clientpayment"} passHref>
               <button className={paymentStyles.buttons}>CLIENT PAYMENTS</button>
             </NextLink>
+          </div>
+          <div className={paymentHistoryStyles.transactionHistory}>
+            <div className={paymentHistoryStyles.dateContainer}>
+              <h1>DATE</h1>
+            </div>
+            <div className={paymentHistoryStyles.dateContainer}></div>
           </div>
         </div>
       </div>
