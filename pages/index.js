a3c8b9be17917f83e134775af77be083ed841521
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2022-01-22 19:03:42 +0800

    debug

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,7 @@ function Home() {
     <div className="p-4">
       <div className="container">
         <h1>Hi {userService.userValue?.firstName}!</h1>
-        <p>You&apos;re logged in with Next.js & JWT!!</p>
-        <p>
-          <Link href="/users">Manage Users</Link>
-        </p>
+        <p>You&apos;re logged in to Tech Assits Web Console </p>
       </div>
     </div>
   );
